a189a0f791c4657cd51825f45cf04f3465efe310
rajanand02 <raj_anand02@yahoo.in>
2013-12-26 16:17:15 +0530

    showing ribbits count and add ability to sign in on account creation

--- app/controllers/ribits_controller.rb
+++ app/controllers/ribits_controller.rb
@@ -1,2 +1,9 @@
 class RibitsController < ApplicationController
-end
+
+  def create
+    ribit = Ribit.new(params[:ribit])
+    ribit.user_id = current_user.id
+    falsh[:error] = "Your Ribbit was over 140 Characters" unless ribit.save
+    redirect_to root_url
+  end
+end 
