c058110d182cf9d0fc84327bc212f13ea96ed508
rajanand02 <raj_anand02@yahoo.in>
2013-12-28 00:32:18 +0530

    remove rabbit.js precompile in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Ribbit::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile = ["ribbits.js"]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
