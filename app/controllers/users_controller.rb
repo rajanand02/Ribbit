5052a07acf55de66f390df6bf0f021e8e16bc682
rajanand02 <raj_anand02@yahoo.in>
2013-12-27 23:17:42 +0530

    add buddies ribbits option to view follower's ribbits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
+    if current_user
+      redirect_to buddies_path
+    else
+      @user = User.new
+    end
   end
 
   def create
@@ -14,6 +18,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+    @ribit = Ribit.new
+  end
+
   def show
     @user = User.find(params[:id])
     @ribit = Ribit.new
@@ -21,5 +30,15 @@ class UsersController < ApplicationController
       follower_id: current_user.id,
       followed_id: @user.id
     ).first_or_initialize if current_user
+  end
+
+  def buddies
+    if current_user
+      @ribit = Ribit.new
+      buddies_ids = current_user.followeds.map(&:id).push(current_user.id)
+      @ribits = Ribit.find_all_by_user_id buddies_ids
+    else
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Ribbit::Application.routes.draw do
   resources :users
 
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'buddies', to: 'users#buddies', as: 'buddies'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
